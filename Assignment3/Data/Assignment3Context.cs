e639c7cfaa4e287a4184ae002e0944b803c8bb1d
zbsan <zbsan@SANTIAGOSLAPW>
2025-03-07 14:06:56 -0500

    Add project files.

---
+++ Assignment3/Data/Assignment3Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using RazorPagesMovie.Models;
+
+namespace Assignment3.Data
+{
+    public class Assignment3Context : DbContext
+    {
+        public Assignment3Context (DbContextOptions<Assignment3Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ Assignment3/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Assignment3.Data;
+using RazorPagesMovie.Models;
+
+namespace Assignment3.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Assignment3.Data.Assignment3Context _context;
+
+        public CreateModel(Assignment3.Data.Assignment3Context context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; } = default!;
+
+        // For more information, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
